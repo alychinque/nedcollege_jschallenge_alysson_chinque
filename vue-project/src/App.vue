d38f502365b5a0823635e3e483b2518150896f11
alychinque <alychinque@gmail.com>
2023-05-09 13:11:21 +0100

    frontend: boiler plate of the application.

---
+++ vue-project/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <header>
+    <div class="wrapper">
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+          <RouterLink class="navbar-brand" to="/">Blog Posts</RouterLink>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <RouterLink class="nav-link active" aria-current="page" to="/"
+                  >Home</RouterLink
+                >
+              </li>
+              <li class="nav-item">
+                <RouterLink class="nav-link" to="/authors">Authors</RouterLink>
+              </li>
+              <li class="nav-item">
+                <RouterLink class="nav-link" to="/login">Login</RouterLink>
+              </li>
+              <li class="nav-item">
+                <RouterLink class="nav-link" to="/registration"
+                  >Register</RouterLink
+                >
+              </li>
+            </ul>
+            <!-- <form class="d-flex" role="search">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-success" type="submit">Search</button>
+            </form> -->
+          </div>
+        </div>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped></style>
---
+++ vue-project/src/views/HomeView.vue
@@ -0,0 +1,104 @@
+<template>
+  <main>
+    <div class="title mt-5 mb-5">
+      <h1>Latest Posts</h1>
+    </div>
+    <div class="header mt-5 mb-5">
+      <h2>Messages</h2>
+    </div>
+    <table>
+      <thead>
+        <tr>
+          <th>Author</th>
+          <th>Title</th>
+          <th>Date</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(post, index) in posts" :key="index">
+          <td>{{ post.author }}</td>
+          <td>
+            <button @click="seePost(post)">Open</button>
+            {{ post.title }}
+          </td>
+          <td>{{ post.date }}</td>
+          <td>
+            <button v-if="user" @click="editPost(post)">Edit</button>
+            <button v-if="user" @click="deletePost(post)">Delete</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <section>
+      <div class="alert alert-warning center" role="alert">
+        Register or Login to add a post
+      </div>
+    </section>
+  </main>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      posts: [
+        {
+          author: "John Doe",
+          title: "My First Post",
+          date: "2022-05-01",
+          content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        },
+        {
+          author: "Jane Smith",
+          title: "My Second Post",
+          date: "2022-05-02",
+          content:
+            "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        },
+        // Add more posts as needed
+      ],
+    };
+  },
+  methods: {
+    editPost(post) {
+      // Implement edit functionality
+    },
+    deletePost(post) {
+      // Implement delete functionality
+    },
+  },
+};
+</script>
+
+<style>
+table {
+  width: 100%;
+  border-collapse: collapse;
+}
+th,
+td {
+  padding: 10px;
+  text-align: left;
+  border-bottom: 1px solid #ddd;
+}
+th {
+  background-color: #f2f2f2;
+  font-weight: bold;
+}
+button {
+  margin-right: 5px;
+}
+@media (max-width: 768px) {
+  th:nth-child(1),
+  td:nth-child(1) {
+    display: none;
+  }
+}
+@media (max-width: 576px) {
+  th:nth-child(3),
+  td:nth-child(3) {
+    display: none;
+  }
+}
+</style>
